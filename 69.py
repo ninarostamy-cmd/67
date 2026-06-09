00a3dad2a25014e955b765115c9f615723c64a5d
ninarostamy-cmd <ninarostamy@gmail.com>
2026-06-09 17:46:11 +0100

    Update 69.py
    hi i bob

--- 69.py
+++ 69.py
@@ -1,14 +1,16 @@
 import random
 
 num1 = random.randint(1, 100)
+num2 = 0
 
 while True:
     hello = input("pick a number :)")
+    num2 += 1
     hello = hello.strip()
     if hello.isdigit():
         hello = int(hello)
         if hello == num1:
-            print("you have to much aura")
+            print(f"you have to much aura.It took bro {num2} tries...")
             break
 
         if hello < num1:
